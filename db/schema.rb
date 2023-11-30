2fcc8021075f157fb64a937f5d2661e7ad94fece
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-11-30 13:55:31 +0300

    Add create_users and post migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_131537) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_104944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comment_tables", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "post_id"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comment_tables_on_author_id"
-    t.index ["post_id"], name: "index_comment_tables_on_post_id"
-  end
-
-  create_table "like_tables", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_like_tables_on_author_id"
-    t.index ["post_id"], name: "index_like_tables_on_post_id"
-  end
-
-  create_table "post_tables", force: :cascade do |t|
-    t.bigint "author_id"
-    t.string "title"
-    t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_post_tables_on_author_id"
-  end
-
-  create_table "user_tables", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
     t.text "bio"
@@ -53,9 +23,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_131537) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comment_tables", "post_tables", column: "post_id"
-  add_foreign_key "comment_tables", "user_tables", column: "author_id"
-  add_foreign_key "like_tables", "post_tables", column: "post_id"
-  add_foreign_key "like_tables", "user_tables", column: "author_id"
-  add_foreign_key "post_tables", "user_tables", column: "author_id"
 end
